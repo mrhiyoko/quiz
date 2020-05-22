9b21fd37e27666adb8630337d830f66cc3a44c1d
mrhiyoko <mgorzkowski@sigma.ug.edu.pl>
2020-05-22 22:18:26 +0200

    upd QuestionBox component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,51 @@
 <template>
   <div id="app">
-    <QuestionBox/>
+    <b-container>
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox/>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import QuestionBox from "@/components/QuestionBox";
+  import QuestionBox from "@/components/QuestionBox";
+  import axios from 'axios';
 
-export default {
-  name: 'app',
-  components: {
-    QuestionBox
+  export default {
+    name: 'app',
+    components: {
+      QuestionBox
+    },
+    data() {
+      return {
+        questions: []
+      }
+    },
+    mounted: function () {
+      axios.get('https://opentdb.com/api.php?amount=10&category=27&type=multiple')
+      .then((response) => {
+        console.log(response.data)
+      })
+      .then((data) => {
+        this.questions = data.results;
+      })
+      .catch(error => {
+        console.log(error)
+      });
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,30 +1,27 @@
 <template>
-    <div>
-        <b-jumbotron>
-            <template v-slot:header>BootstrapVue</template>
+  <div>
+    <b-jumbotron>
 
-            <template v-slot:lead>
-                This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
-                featured content or information.
-            </template>
+      <template v-slot:lead>
+        Question
+      </template>
 
-            <hr class="my-4">
+      <hr class="my-4">
 
-            <p>
-                It uses utility classes for typography and spacing to space content out within the larger
-                container.
-            </p>
+      <p>
+        list
+      </p>
 
-            <b-button variant="primary" href="#">Do Something</b-button>
-            <b-button variant="success" href="#">Do Something Else</b-button>
-        </b-jumbotron>
-    </div>
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "QuestionBox"
-    }
+  export default {
+    name: "QuestionBox"
+  }
 </script>
 
 <style scoped>
