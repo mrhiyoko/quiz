972dcc494fe2f1caf9ccc89160cc261074a06eec
mrhiyoko <mgorzkowski@sigma.ug.edu.pl>
2020-05-22 18:03:21 +0200

    upd Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,16 @@
 <template>
-    <h1>Header</h1>
+  <div>
+    <b-nav>
+      <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+      <b-nav-item disabled>Counter</b-nav-item>
+    </b-nav>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Header"
-    }
+  export default {
+    name: "Header"
+  }
 </script>
 
 <style scoped>
